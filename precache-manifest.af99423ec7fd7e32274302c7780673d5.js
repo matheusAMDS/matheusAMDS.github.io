6675bea55bd7c29bc85437c79f14d8b4c4aa7bf9
matheusAMDS <matheusa56@gmail.com>
2019-10-25 14:14:42 -0300

    Updates

---
+++ precache-manifest.af99423ec7fd7e32274302c7780673d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a534aee884c974fb94a9fa927d54d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e8897de1be77e5717e",
+    "url": "/static/css/main.66bb6c9e.chunk.css"
+  },
+  {
+    "revision": "e104ff9441e0b5eb9f35",
+    "url": "/static/js/2.c2e077af.chunk.js"
+  },
+  {
+    "revision": "04e8897de1be77e5717e",
+    "url": "/static/js/main.a56aa964.chunk.js"
+  },
+  {
+    "revision": "0b299031e879744095e6",
+    "url": "/static/js/runtime-main.6928400b.js"
+  },
+  {
+    "revision": "0e7feb70f12f36a9cdce2e0033fc742a",
+    "url": "/static/media/magnifying-glass.0e7feb70.svg"
+  }
+]);
